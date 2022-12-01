357e00318bffaffc81560910bde888f2a0d74f15
littlezero48 <littlezero.048@gmail.com>
2022-12-01 11:59:26 +0900

    깃헙 레파지토리 테스트

--- assignment_createAPI_test/src/main/java/com/example/assignment_createapi_test/repository/MemberRepository.java
+++ assignment_createAPI_test/src/main/java/com/example/assignment_createapi_test/repository/MemberRepository.java
@@ -3,7 +3,5 @@ package com.example.assignment_createapi_test.repository;
 import com.example.assignment_createapi_test.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MemberRepository extends JpaRepository<Member,Long> {
 }
---
+++ assignment_test2/src/main/java/com/example/assignment_test2/AssignmentTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.assignment_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentTest2Application.class, args);
+    }
+
+}
