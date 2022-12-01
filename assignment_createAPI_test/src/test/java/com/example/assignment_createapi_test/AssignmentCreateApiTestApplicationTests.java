03f89c94a9f69f8ee623169e9dac1c9c02928954
littlezero48 <littlezero.048@gmail.com>
2022-12-01 10:20:46 +0900

    기본세팅 커밋

---
+++ assignment_createAPI_test/src/test/java/com/example/assignment_createapi_test/AssignmentCreateApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_createapi_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentCreateApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
