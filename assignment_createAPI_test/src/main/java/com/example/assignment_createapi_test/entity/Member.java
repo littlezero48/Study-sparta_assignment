afcd76125914a3e7ea689d91150043283073612b
littlezero48 <littlezero.048@gmail.com>
2022-12-01 10:12:15 +0900

    테스트 시작

---
+++ assignment_createAPI_test/src/main/java/com/example/assignment_createapi_test/entity/Member.java
@@ -0,0 +1,24 @@
+package com.example.assignment_createapi_test.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String pw;
+}
--- assignment_memo/src/main/java/com/example/assignment_memo/controller/MemoController.java
+++ assignment_memo/src/main/java/com/example/assignment_memo/controller/MemoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController             // 컨트롤러 선언
-@RequiredArgsConstructor    // 생성자 자동 주입
+@RequiredArgsConstructor    // final 변수, Notnull 표시가 된 변수처럼 필수적인 정보를 세팅하는 생성자를 만든다.
 public class MemoController {
 
     private final MemoService memoService;
