e6da8cc7f4a23b2a7072c9f76ad0a967b9e311af
littlezero48 <littlezero.048@gmail.com>
2022-12-04 22:34:04 +0900

    first commit

---
+++ assignment_blogBackend/src/main/java/com/example/assignment_blogbackend/AssignmentBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_blogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentBlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentBlogBackendApplication.class, args);
+    }
+
+}
---
+++ assignment_blogBackend/src/test/java/com/example/assignment_blogbackend/AssignmentBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_blogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentBlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
