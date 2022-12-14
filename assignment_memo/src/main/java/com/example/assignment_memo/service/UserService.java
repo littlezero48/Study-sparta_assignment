4cd452def0346dd680546c5b709d36366f9d087b
littlezero48 <littlezero048@email.com>
2022-12-15 02:34:17 +0900

    UserService 주석 정리

--- assignment_memo/src/main/java/com/example/assignment_memo/service/UserService.java
+++ assignment_memo/src/main/java/com/example/assignment_memo/service/UserService.java
@@ -14,8 +14,8 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 
-import static com.example.assignment_memo.util.error.ErrorCode.EXIST_USER;
-import static com.example.assignment_memo.util.error.ErrorCode.LOGIN_MATCH_FAIL;
+import static com.example.assignment_memo.util.ApiResponse.CodeError.EXIST_USER;
+import static com.example.assignment_memo.util.ApiResponse.CodeError.LOGIN_MATCH_FAIL;
 
 @Service
 @RequiredArgsConstructor
@@ -23,35 +23,48 @@ public class UserService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
+    // 가입시 작성하면 관리자로 인정 받는 코드
     private static final String ADMIN_TOKEN = "admin";
 
+    // 회원가입
     public MessageDto signup(SignupRequestDto dto){
+
+        // 1. 응답으로부터 가입 정보 받기
         String username = dto.getUsername();
         String password = dto.getPassword();
+
+        // 2. 권한 체크
         UserRoleEnum role = ADMIN_TOKEN.equals(dto.getAdminToken()) ? UserRoleEnum.ADMIN : UserRoleEnum.USER ;
 
+        // 3. 동일한 유저 존재여부 체크
         if(userRepository.findByUsername(username).isPresent()){
             throw new CustomException(EXIST_USER);
         };
 
+        // 4. 가입 정보 저장
         User user = new User(username, password, role);
         userRepository.save(user);
 
+        // 5. 성공 메세지 리턴
         return new MessageDto(StatusEnum.OK);
     }
 
+    // 로그인
     public MessageDto login(LoginRequestDto dto, HttpServletResponse response){
+
+        // 1. 응답으로부터 로그인 정보 받기
         String username = dto.getUsername();
         String password = dto.getPassword();
 
+        // 2. 아이디와 비밀번호 일치여부 확인
         User user = userRepository.findByUsernameAndPassword(username, password).orElseThrow(
                 ()-> new CustomException(LOGIN_MATCH_FAIL)
         );
 
-        // HTTP Header에 생성한 토큰을 붙여 보내기!
-        //Optional은 값이 없을 때 값을 쓰려고 하면 예외 발생
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(),  user.getRole()));  // 메소드사용하려면 의존성주입 먼저
+        // 3. 응답 헤더에 토큰 세팅
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(),  user.getRole()));     // HTTP Header에 생성한 토큰을 세팅 //Optional은 값이 없을 때 값을 쓰려고 하면 예외 발생
 
+        // 4. 성공 메세지 리턴
         return new MessageDto(StatusEnum.OK);
     }
 }
